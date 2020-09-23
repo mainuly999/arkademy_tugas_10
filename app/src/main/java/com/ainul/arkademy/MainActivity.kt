fac721daaaf873bae955093496eb453a0db67d65
mainuly999 <mainuly999@gmail.com>
2020-09-23 23:05:46 +0800

    loginScreen

--- app/src/main/java/com/ainul/arkademy/MainActivity.kt
+++ app/src/main/java/com/ainul/arkademy/MainActivity.kt
@@ -2,8 +2,14 @@ package com.ainul.arkademy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var masuk: Button
+    private lateinit var daftar: Button
+    private lateinit var inputUsername: TextView
+    private lateinit var inputPassword: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
